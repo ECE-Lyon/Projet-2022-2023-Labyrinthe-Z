d0ee606646921362a5050db7f28ac687ddec7a9a
Roomey69 <zamedesbonnett@gmail.com>
2022-12-01 21:14:51 +0100

    Test afficher une image dans une fenetre

--- src/testsdl.c
+++ src/testsdl.c
@@ -7,28 +7,23 @@ void SDL_ExitError(const char *message);
 
 int main(int argc, char **argv)
 {
-    
-    SDL_version nb;
-    SDL_VERSION(&nb);
-
-    printf("\nBienvenue sur la version SDL %d.%d.%d !\n", nb.major,nb.minor,nb.patch);
 
     SDL_Window *window;
     SDL_Renderer *rendu;
 
-    if( SDL_Init(SDL_INIT_VIDEO) != 0 )
+    if(SDL_Init(SDL_INIT_VIDEO))
         SDL_ExitError("Erreur initialisation video");
 
-    if(SDL_CreateWindowAndRenderer(1920,1080, SDL_RENDERER_ACCELERATED, &window, &rendu) != 0) // CREE UNE FENETRE ET UN RENDU
+    if(SDL_CreateWindowAndRenderer(1920,1080, SDL_RENDERER_ACCELERATED, &window, &rendu)) // CREE UNE FENETRE ET UN RENDU
         SDL_ExitError("Erreur de fenetre et rendu");
 
-    if(SDL_SetRenderDrawColor(rendu,82,192,213, SDL_ALPHA_OPAQUE) != 0) // SET COULEUR POUR DESSINER
+    /*if(SDL_SetRenderDrawColor(rendu,82,192,213, SDL_ALPHA_OPAQUE)) // SET COULEUR POUR DESSINER
         SDL_ExitError("Erreur drawcolor pour rendu");
 
-    if(SDL_RenderDrawPoint(rendu, 960,540) != 0) // DESSINE UN POINT EN COORD 940/540 (au centre)
+    if(SDL_RenderDrawPoint(rendu, 960,540)) // DESSINE UN POINT EN COORD 940/540 (au centre)
         SDL_ExitError("Erreur dessin point");
 
-    if(SDL_RenderDrawLine(rendu,0,0,1920,1080) != 0) // DESSINE UNE LIGNE DEPUIS 0/0 à 1920/1080
+    if(SDL_RenderDrawLine(rendu,0,0,1920,1080)) // DESSINE UNE LIGNE DEPUIS 0/0 à 1920/1080
         SDL_ExitError("Erreur dessin ligne");
 
     SDL_Rect rectangle;
@@ -37,21 +32,69 @@ int main(int argc, char **argv)
     rectangle.h = 120;
     rectangle.w = 200;
 
-    if(SDL_RenderDrawRect(rendu,&rectangle) != 0)
+    if(SDL_RenderFillRect(rendu,&rectangle) != 0) // REMPLI
         SDL_ExitError("Erreur dessin rectangle");
 
-    SDL_RenderPresent(rendu); // ACTIVE LE RENDU
+    if(SDL_SetRenderDrawColor(rendu,190,245,255, SDL_ALPHA_OPAQUE)) // SET COULEUR POUR DESSINER
+        SDL_ExitError("Erreur drawcolor pour rendu");
+
+    if(SDL_RenderDrawRect(rendu,&rectangle) != 0) // CONTOUR 
+        SDL_ExitError("Erreur dessin rectangle");
+
+    SDL_RenderPresent(rendu); // ACTIVE LE RENDU  */
+
+    SDL_Surface *image = NULL;
+    SDL_Texture *texture = NULL;
+
+    image = SDL_LoadBMP("src/test.bmp");
+
+    if(image){
+        SDL_DestroyRenderer( rendu );
+        SDL_DestroyWindow( window );
+        SDL_ExitError("Erreur de chargement de l'image");
+    }
+
+    texture = SDL_CreateTextureFromSurface(rendu, image);
+    SDL_FreeSurface(image);
+
+    if(texture){
+        SDL_DestroyRenderer(rendu);
+        SDL_DestroyWindow(window);
+        SDL_ExitError("Erreur de création de la texture");
+    }
+
+    SDL_Rect rectangle;
+    rectangle.h = 257;
+    rectangle.w = 261;
+
+    if(SDL_QueryTexture(texture, NULL, NULL, &rectangle.w, &rectangle.h)){
+        SDL_DestroyRenderer(rendu);
+        SDL_DestroyWindow(window);
+        SDL_ExitError("Erreur de chargement de la texture");
+    }
+
+    rectangle.x = (261 - rectangle.w) / 2;
+    rectangle.y = (257 - rectangle.h) / 2;
+
+    if(SDL_RenderCopy(rendu, texture, NULL, &rectangle)){
+        SDL_DestroyRenderer(rendu);
+        SDL_DestroyWindow(window);
+        SDL_ExitError("Erreur d'afficher la texture");        
+    }
+
+    SDL_RenderPresent(rendu);
 
     SDL_Delay(5000);
 
-    SDL_DestroyRenderer( rendu );
-    SDL_DestroyWindow( window );
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(rendu);
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
 
 void SDL_ExitError(const char *message){
-    SDL_Log("Erreur d'initialisation SDL > %s\n",SDL_GetError());
+    SDL_Log("%s > %s\n",message,SDL_GetError());
     exit(EXIT_FAILURE);
     SDL_Quit();
 }
