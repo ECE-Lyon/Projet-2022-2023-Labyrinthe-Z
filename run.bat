2125e43a20b29b79681664895a566f4ba8cc8beb
nolan6363 <nolan.bayon@gmail.com>
2023-01-20 10:51:29 +0100

    fix du .bat

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-gcc src/main_interface.c -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_mixer ;  bin\prog
+gcc src/main_interface.c -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_mixer
+bin\prog
